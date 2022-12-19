f3972f896c33ddaeb49910fbc7e591dcfb742810
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-12-19 13:12:06 -0500

    added inquirer for giving the user options for either searching for a book or viewing the saved booklist

--- src/app.js
+++ src/app.js
@@ -2,6 +2,28 @@ const inquirer = require('inquirer')
 
 const main = async () => {
     console.log("Welcome to the BookSearchApp!")
+    const action = await getAction()
+    if (action === "search") {
+        // make a call to Google Books API
+    } else if (action === "view") {
+        // print a list of all saved books
+    }
+}
+
+// This function lets the user select an option
+const getAction = async() => {
+    const answer = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: "What would you like to do?",
+            choices: [
+                {value: "search", name: "Search Google Books"},
+                {value: "view", name: "View saved reading list"}
+            ]
+        }
+    ])
+    return answer.action
 }
 
 main()
